c8a29ba70f43368398c47a2ec11aeab4baab2477
Jose Mora <jose.mora@mindtree.com>
2017-08-11 10:24:40 -0400

    Components creation

---
+++ src/app/advertising/advertising.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advertising',
+  templateUrl: './advertising.component.html',
+  styleUrls: ['./advertising.component.sass']
+})
+export class AdvertisingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/league-list/league-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-league-list',
+  templateUrl: './league-list.component.html',
+  styleUrls: ['./league-list.component.sass']
+})
+export class LeagueListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
