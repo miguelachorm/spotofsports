3033c9cea98117bd9f6ffa28bc43cb5a862220b5
Jose Mora <jose.mora@mindtree.com>
2017-08-28 12:01:27 -0400

    adding styling, add component carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdChipsModule, MdToolbarModule, MdIconModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';;
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +12,8 @@ import { LeagueListComponent } from './league-list/league-list.component';
 import { LeagueDetailComponent } from './league-detail/league-detail.component';
 import { AdvertisingComponent } from './advertising/advertising.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -17,12 +22,30 @@ import { FooterComponent } from './footer/footer.component';
     LeagueListComponent,
     LeagueDetailComponent,
     AdvertisingComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule, 
+    MdCheckboxModule,
+    MdChipsModule,
+    MdToolbarModule,
+    MdIconModule,
+    RouterModule.forRoot([
+      {path: 'league-list', component: LeagueListComponent},
+      {path: 'league-detail', component: LeagueDetailComponent}
+    ])
+  ],
+  exports: [
+    MdButtonModule, 
+    MdCheckboxModule,
+    MdChipsModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
